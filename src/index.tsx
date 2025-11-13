bd08b2852fe7cb9a0b70d73b6e25aa56802316f1
sivaram000 <katta.sivaram@suprsend.com>
2025-11-13 18:04:12 +0530

    added prefix for styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
-import { Button } from '@/components/Button';
+import { Button } from '@/components/ui/button';
 import './index.css';
 
 export function MyButton() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold underline bg-red-600">Hello World</h1>
+    <div className="suprsend-flex suprsend-min-h-svh suprsend-flex-col suprsend-items-center suprsend-justify-center">
+      <h1 className="suprsend-text-3xl suprsend-font-bold suprsend-underline suprsend-bg-red-600">
+        Hello World
+      </h1>
       <Button>Click me default</Button>
     </div>
   );
