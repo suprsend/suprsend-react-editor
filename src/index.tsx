25e77ce561bb7a2c966e8e35ca01b5965db0cfa0
sivaram000 <katta.sivaram@suprsend.com>
2026-02-26 19:01:38 +0530

    code structure changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line react-refresh/only-export-components
 export * from '@/types';
-export { default as TemplateEditor } from '@/components/TemplateEditor';
-export { default as CommitButton } from '@/components/Commit';
-export { default as SuprSendTemplateEditorProvider } from '@/components/SuprSendTemplateEditorProvider';
+export { default as TemplateEditor } from '@/templates/TemplateEditor';
+export { default as CommitButton } from '@/templates/Commit';
+export { default as SuprSendTemplateEditorProvider } from '@/components/SuprSendEditorProvider';
